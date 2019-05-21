6f19e231115ed4b93056bd3a5add28161a7009d6
Sina <Sina.baharlou@gmail.com>
2019-05-22 00:10:10 +0430

    Enhanced the coding style and comments.

--- VR/src/main/java/com/deepsoft/vehiclerecognition/Utils.java
+++ VR/src/main/java/com/deepsoft/vehiclerecognition/Utils.java
@@ -1,13 +1,3 @@
-package com.deepsoft.vehiclerecognition;
-
-import android.content.Context;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Vector;
-
 /**
  * Vehicle Recognition Project
  * Associated with the paper "Transfer Learning Approach for Classification and Noise Reduction on Noisy Web data"
@@ -15,46 +5,54 @@ import java.util.Vector;
  * Project page: https://www.sinabaharlou.com/VehicleRecognition
  */
 
+// -- Package declaration -- 
+package com.deepsoft.vehiclerecognition;
+
+// -- Import required libraries --
+import android.content.Context;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Vector;
+
+// -- Utils Class -- 
 class Utils
 {
-
-
+    // -- Load json file --
     static String loadJson(Context context, String filename) throws IOException
     {
-
-        // get json file input stream
+        // -- Get json file input stream --
         InputStream inputStream = context.getAssets().open(filename);
 
-        // get file size and allocate buffer
+        // -- Get file size and allocate the buffer --
         int fileSize = inputStream.available();
         byte[] inputBuffer = new byte[fileSize];
 
-        // read the file and close it
+        // -- Read the file and close it --
         inputStream.read(inputBuffer);
         inputStream.close();
 
         return new String(inputBuffer, "UTF-8");
     }
 
+    // -- Load labels file --
     static Vector<String> loadLabels(Context context, String filename) throws IOException
     {
-
-        // get file input stream
+        // -- Get the file input stream --
         InputStream inputStream = context.getAssets().open(filename);
         BufferedReader reader=new BufferedReader(new InputStreamReader(inputStream));
         Vector<String> labels= new Vector<>();
 
-        // fetch labels line by line
+        // -- Fetch labels line by line --
         String inputLine;
         while((inputLine=reader.readLine())!=null)
             labels.add(inputLine);
 
-        // close file
+        // -- Close the file --
         reader.close();
         inputStream.close();
 
         return labels;
     }
-
-
 }
